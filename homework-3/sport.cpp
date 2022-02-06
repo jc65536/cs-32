7057a7425e4647c3fe00942ce81cae83606e3081
Jason Cheng <jasoncheng224@gmail.com>
2022-02-06 15:16:40 -0800

    clean sport

--- homework-3/sport.cpp
+++ homework-3/sport.cpp
@@ -1,39 +1,48 @@
-#include <iostream>
-#include <string>
-
 class Sport {
 public:
-    Sport(std::string sportName) : sportName(sportName) {}
-    std::string name() const { return sportName; };
+    Sport(string sportName) : sportName(sportName) {}
+    string name() const {
+        return sportName;
+    };
     virtual bool isOutdoor() const = 0;
-    virtual std::string icon() const = 0;
-    void destructorMsg(std::string className, std::string addendum = "") const {
-        std::cout << "Destroying the " << className
-                  << " object named " << name()
-                  << addendum << "." << std::endl;
+    virtual string icon() const = 0;
+    void destructorMsg(string className, string addendum = "") const {
+        cout << "Destroying the " << className
+             << " object named " << name()
+             << addendum << "." << endl;
     }
     virtual ~Sport() {}
 
 private:
-    std::string sportName;
+    string sportName;
 };
 
 class Snowboarding : public Sport {
 public:
-    Snowboarding(std::string sportName) : Sport(sportName) {}
-    bool isOutdoor() const { return true; }
-    std::string icon() const { return "a descending snowboarder"; }
-    ~Snowboarding() { destructorMsg("Snowboarding"); }
+    Snowboarding(string sportName) : Sport(sportName) {}
+    bool isOutdoor() const {
+        return true;
+    }
+    string icon() const {
+        return "a descending snowboarder";
+    }
+    ~Snowboarding() {
+        destructorMsg("Snowboarding");
+    }
 };
 
 class Biathlon : public Sport {
 public:
-    Biathlon(std::string sportName, double distance) : Sport(sportName),
-                                                       distance(distance) {}
-    bool isOutdoor() const { return true; }
-    std::string icon() const { return "a skier with a rifle"; }
+    Biathlon(string sportName, double distance) : Sport(sportName),
+                                                  distance(distance) {}
+    bool isOutdoor() const {
+        return true;
+    }
+    string icon() const {
+        return "a skier with a rifle";
+    }
     ~Biathlon() {
-        std::string distStr = std::to_string(distance);
+        string distStr = to_string(distance);
         distStr.erase(distStr.find_last_not_of('0') + 1);
         destructorMsg("Biathlon", ", distance " + distStr + " km");
     }
@@ -44,8 +53,14 @@ private:
 
 class FigureSkating : public Sport {
 public:
-    FigureSkating(std::string sportName) : Sport(sportName) {}
-    bool isOutdoor() const { return false; }
-    std::string icon() const { return "a skater in the Biellmann position"; }
-    ~FigureSkating() { destructorMsg("FigureSkating"); }
+    FigureSkating(string sportName) : Sport(sportName) {}
+    bool isOutdoor() const {
+        return false;
+    }
+    string icon() const {
+        return "a skater in the Biellmann position";
+    }
+    ~FigureSkating() {
+        destructorMsg("FigureSkating");
+    }
 };
