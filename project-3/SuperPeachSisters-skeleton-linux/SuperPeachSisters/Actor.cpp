9638220cc69489cc28e1e92295ab74924f2dbd95
Jason Cheng <jasoncheng224@gmail.com>
2022-02-15 12:01:34 -0800

    actor, peach, block classes

--- project-3/SuperPeachSisters-skeleton-linux/SuperPeachSisters/Actor.cpp
+++ project-3/SuperPeachSisters-skeleton-linux/SuperPeachSisters/Actor.cpp
@@ -1,4 +1,13 @@
 #include "Actor.h"
+#include "GameConstants.h"
 #include "StudentWorld.h"
 
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+Actor::Actor(int imageId, double startX, double startY,
+             int startDirection = 0, double size = 1.0, int depth = 0)
+    : GraphObject(imageId, startX, startY, startDirection, depth, size) {}
+
+Peach::Peach() : Actor(IID_PEACH, 0, 0) {}
+
+Block::Block() : Actor(IID_BLOCK, 4, 4) {}
--- project-3/SuperPeachSisters-skeleton-linux/SuperPeachSisters/Actor.h
+++ project-3/SuperPeachSisters-skeleton-linux/SuperPeachSisters/Actor.h
@@ -5,4 +5,20 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class Actor : public GraphObject {
+public:
+    Actor(int imageId, double startX, double startY,
+          int startDirection = 0, double size = 1.0, int depth = 0);
+};
+
+class Peach : public Actor {
+public:
+    Peach();
+};
+
+class Block : public Actor {
+public:
+    Block();
+};
+
 #endif // ACTOR_H_
